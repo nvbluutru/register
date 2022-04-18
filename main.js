ee1413e20c21182c2e9641fc11aca66e5c8d0f44
Bảo Nguyễn Văn <nvbluutru@gmail.com>
2022-04-18 22:12:05 +0700

    done

--- main.js
+++ main.js
@@ -26,6 +26,15 @@ const checkChecked = (selector, elementParent) => {
     elementParent.style.background = "rgb(255 244 156)";
     return true;
 }
+const checkLength = (element, cb)=>{
+    if(cb(element.value.trim())){
+        element.style.background = "rgb(255 244 156)"
+        element.focus();
+        return true;
+    }
+    element.style.background = "white"
+    return false
+}
 let timeOut;
 const showMessage = (value, duration)=>{
     const message = getEle(".message");
@@ -50,8 +59,12 @@ const handleSubmit = (e)=>{
     const gender = getEle("#gender_form");
     if(checkEmpty(id)){
         showMessage("Mã sinh viên không được bỏ trống", 3000);
+    }else if(checkLength(id, (value) => { return value.length !== 7})){
+        showMessage("Mã sinh viên phải nhập 7 kí tự", 3000);
     }else if(checkEmpty(fullName)){
         showMessage("Họ và tên không được bỏ trống", 3000);
+    }else if(checkLength(fullName, (value) => { return value.length < 6 || value.length > 30})){
+        showMessage("Họ và tên phải từ 6 đến 30 kí tự", 3000);
     }else if(checkChecked("gender", gender)){
         showMessage("Vui lòng chọn giới tính", 3000)
     }else if(checkChecked("interests", interests)){
