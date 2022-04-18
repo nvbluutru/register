456590c10db6d5fd8f8229f9452296a4db5bd46e
Bảo Nguyễn Văn <nvbluutru@gmail.com>
2022-04-18 21:54:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const getEle = (selector)=>{
+    return document.querySelector(selector);
+}
+const checkEmpty = (element) => {
+    if(element){
+        const value = element.value;
+        console.log(value)
+        if(value.trim() == ""){
+            element.style.background = "rgb(255 244 156)"
+            element.focus();
+            return true;
+        }else{
+            element.style.background = "white"
+        }
+    }
+    return false;
+}
+const checkChecked = (selector, elementParent) => {
+    const arrayOptions = document.getElementsByName(selector);
+    for(let item of arrayOptions){
+        if(item.checked){
+            elementParent.style.background = "white"
+            return false;
+        }
+    }
+    elementParent.style.background = "rgb(255 244 156)";
+    return true;
+}
+let timeOut;
+const showMessage = (value, duration)=>{
+    const message = getEle(".message");
+    if(message){
+        message.innerHTML = value;
+        message.classList.add("active");
+    }
+    if(timeOut){
+       clearInterval(timeOut);
+    }
+    timeOut = setTimeout(()=>{
+        message.innerHTML = "";
+        message.classList.remove("active");
+    }, duration)
+}
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    const id = getEle("#code");
+    const fullName = getEle("#fullName");
+    const province = getEle("#province");
+    const interests = getEle("#interests_form");
+    const gender = getEle("#gender_form");
+    if(checkEmpty(id)){
+        showMessage("Mã sinh viên không được bỏ trống", 3000);
+    }else if(checkEmpty(fullName)){
+        showMessage("Họ và tên không được bỏ trống", 3000);
+    }else if(checkChecked("gender", gender)){
+        showMessage("Vui lòng chọn giới tính", 3000)
+    }else if(checkChecked("interests", interests)){
+        showMessage("Vui lòng chọn ít nhất 1 sở thích", 3000)
+    }else if(checkEmpty(province)){
+        showMessage("Vui lòng chọn tỉnh", 3000);
+    }else{
+        location.href = "/dangkythanhcong.html"
+    }
+}
+getEle(".form").addEventListener("submit", handleSubmit)
